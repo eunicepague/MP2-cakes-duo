7f3c4c9e2b573d3f9a1a3cccf6f50c3dcdcb93e3
ikecabrido <russcabz28@gmail.com>
2023-09-21 09:26:18 +0800

    added footer

--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const Footer = () => {
-  return <div></div>;
+  return (
+    <Footer>
+      <Container>
+        <Row>
+          <Col>
+            <h3>Quick Links</h3>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="cakes">Cakes</Link>
+            <Link to="/contacts">Contacts</Link>
+          </Col>
+          <Col>
+            <h3></h3>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </Footer>
+  );
 };
 
 export default Footer;
