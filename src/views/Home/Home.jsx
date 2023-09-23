d1661c922924ac7cd4f00625f0a9e2ff8744080f
eunicepague <eunicegracepague@gmail.com>
2023-09-23 11:38:55 +0800

    fix the card selection on section one

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,4 @@
-
-// import React from 'react';
+// import React from 'react'
 import Hero from './sections/Hero';
 import SectionOne from './sections/SectionOne';
 import SectionThree from './sections/SectionThree';
@@ -7,13 +6,13 @@ import AdvertisementOne from './sections/AdvertisementOne';
 
 const Home = () => {
   return (
-    <>
+    <div>
       <Hero />
       <SectionOne />
       <SectionThree />
       <AdvertisementOne />
-    </>
+    </div>
   );
-
+};
 
 export default Home;
