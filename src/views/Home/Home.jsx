37ca6260de13a49b27e2ba9c83f2cd09840f7707
ikecabrido <142366002+ikecabrido@users.noreply.github.com>
2023-09-25 14:26:21 +0800

    Russell branch (#4)
    * added footer content, added login page
    
    * conflicts in css of almost all containers
    
    * added img folder, login, signin content
    
    * tried to fix login page
    
    ---------
    
    Co-authored-by: Eunice <102943525+eunicepague@users.noreply.github.com>

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,11 @@
+
 // import React from 'react'
 import Hero from './sections/Hero';
 import SectionOne from './sections/SectionOne';
 import SectionThree from './sections/SectionThree';
 import AdvertisementOne from './sections/AdvertisementOne';
 import SectionFour from './sections/SectionFour';
+
 
 const Home = () => {
   return (
